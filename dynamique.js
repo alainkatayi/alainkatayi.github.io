4bb5c94e826dfad8db6f63ede899e894d29025a8
alainMH <alainkatayi01@gmail.com>
2024-12-13 00:38:44 +0100

    finition et ajout de la deuxieme image de la partie mes réalisations

--- dynamique.js
+++ dynamique.js
@@ -1,15 +1,17 @@
 var menu = document.getElementById("burger");
 menu.addEventListener('click', Derouler);
 
+
 function Derouler(){
     document.getElementById("menu-deroulant").style.display = "block";
 }
 
+
 var fermer = document.getElementById("fermer");
 fermer.addEventListener('click', fermermenu);
 
+
 function fermermenu(){
     document.getElementById("menu-deroulant").style.display = "none";
-    document.getElementById("burger").style.display = "block";
-    
+    document.getElementById("burger").style.display = "block";  
 }
